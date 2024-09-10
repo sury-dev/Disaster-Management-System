af02484cee1c5ad899bfee9e54762e02dcffa335
Chetan Singh <chet4nsingh@gmail.com>
2024-09-10 15:56:26 +0530

    Update User Dashboard

--- client/src/components/Dashboard/Footer.jsx
+++ client/src/components/Dashboard/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <section className="py-4 px-8 h-3/4 grid grid-cols-3 gap-4 mt-4">
+    <section className="py-4 px-8 h-3/4 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 mt-4">
       <a className="flex gap-8 border-2 border-dark-purple-x rounded-3xl items-center p-8 box-border cursor-pointer py-4 bg-white text-dark-purple group">
         <i className="fas fa-exclamation-triangle mr-2 text-[#FF6347] text-6xl group-hover:scale-110 duration-100"></i>
         <div>
@@ -28,15 +28,4 @@ export default function Footer() {
       </a>
     </section>
   );
-}
-
-{
-  /* <a className="flex justify-between items-center font-bold p-8 box-border cursor-pointer hover:border-b-2 border-dark-purple text-right py-4 text-dark-purple text-3xl group">
-        <i className="fas fa-hand-holding-heart mr-2 text-[#32CD32] text-6xl group-hover:scale-110 duration-100"></i>
-        <span>Donate To Rescue Camps</span>
-      </a>
-      <a className="flex justify-between items-center font-bold p-8 box-border cursor-pointer hover:border-b-2 border-dark-purple text-right py-4 text-dark-purple text-3xl group">
-        <i className="fas fa-hands-helping mr-2 text-[#FFD700] text-6xl group-hover:scale-110 duration-100"></i>
-        <span>Volunteer</span>
-      </a> */
 }
